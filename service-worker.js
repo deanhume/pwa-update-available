b0e6bb1017b83e8aa48539fa6c82c85bfef21d34
deanhume <dean@settled.co.uk>
2017-12-05 14:58:15 +0000

    Nailed it :+1:

--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,29 @@
-var cacheName = 'basicCache9';
+var cacheName = 'basicCacheo';
 
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(cacheName)
-    .then(cache => cache.addAll([
-      './dog-face.jpg'
-    ]))
+      .then(cache => cache.addAll([
+        './dog-face.jpg'
+      ]))
   );
 });
 
-self.addEventListener('fetch', function(event) {
+self.addEventListener('message', function (event) {
+  if (event.data.action === 'skipWaiting') {
+    console.log('Inside Service Worker', event.data.action);
+    self.skipWaiting();
+  }
+});
+
+self.addEventListener('fetch', function (event) {
   event.respondWith(
     caches.match(event.request)
-    .then(function(response) {
-      if (response) {
-        return response;
-      }
-      return fetch(event.request);
-    })
+      .then(function (response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
   );
 });
